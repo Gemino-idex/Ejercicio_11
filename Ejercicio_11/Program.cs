bc7a57adb821c9d7f7e0c3cec3be95149a7e8795
Gemino <71484444@idexperujapon.edu.pe>
2025-06-20 18:24:46 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=0;
+            do
+            {
+                Console.WriteLine("Ingrese su contraseña:");
+                x = Convert.ToInt32(Console.ReadLine());
+            } while (x != 1234);
+            Console.WriteLine("Bienvenido de nuevo");
+
+        }
+    }
+}
