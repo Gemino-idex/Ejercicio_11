7f2882576697198a9ef07f201459d2269e509557
Gemino <71484444@idexperujapon.edu.pe>
2025-06-26 17:39:58 -0500

    finalizado

--- Ejercicio_11/Program.cs
+++ Ejercicio_11/Program.cs
@@ -10,11 +10,14 @@ namespace Ejercicio_11
     {
         static void Main(string[] args)
         {
+            //declaramos variables
             string x;
+            //do es para repetir una acción pero se evalue al final en while
             do
             {
                 Console.WriteLine("Ingrese su contraseña:");
                 x = Console.ReadLine();
+                //while va al final para verificar si la información es correcta no se vuelve a ejecutar
             } while (x != "1234");
             Console.WriteLine("Bienvenido de nuevo");
 
